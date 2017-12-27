52c41610f17a2b82d16d7acdf385f842adbec195
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2017-12-27 21:39:42 +0900

    using lable & break gugudan

--- JavaStudy/src/com/youjin/label/Main.java
+++ JavaStudy/src/com/youjin/label/Main.java
@@ -2,17 +2,13 @@ package com.youjin.label;
 
 public class Main {
 	public static void main(String[] args) {
-		boolean sw = true;
-		for (int i = 1; i <= 9; i++) {
+		gugudan :for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
 				if (j == 4 && i == 4) {
-					sw = false;
-					break;
+					break gugudan;
 				}
 				System.out.println(i + "X" + j + "=" + i * j);
 			}
-			if (sw == false)
-				break;
 		}
 	}
 }
