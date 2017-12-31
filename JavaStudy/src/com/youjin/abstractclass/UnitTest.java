9a4fd6686c2aea55964078c6e73b9e6427ab4520
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2017-12-31 22:13:40 +0900

    why error occur

--- JavaStudy/src/com/youjin/abstractclass/UnitTest.java
+++ JavaStudy/src/com/youjin/abstractclass/UnitTest.java
@@ -3,6 +3,7 @@ package com.youjin.abstractclass;
 public class UnitTest {
 
 	public static void main(String[] args) {
+//		Object[] group = new Object[4];		//이렇게 선언하면 14line에서 오류가 난다. //Object클래스에는 Move함수가 정의되어있지 않기 때문이다.
 		Unit[] group = new Unit[4];
 		group[0] = new Marine();
 		group[1] = new Tank();
