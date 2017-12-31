a59d0962e907610b16b5ecd713d163b0f4f2ac94
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2017-12-31 11:18:47 +0900

    Block Test

---
+++ JavaStudy/src/com/youjin/initialization/StaticBlockTest.java
@@ -0,0 +1,19 @@
+package com.youjin.initialization;
+
+public class StaticBlockTest {
+	static int[] arr = new int[10];
+	
+	static {
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = (int)(Math.random()*10) + 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("arr["+i+"] :" + arr[i]);
+		}
+		
+	}
+
+}
