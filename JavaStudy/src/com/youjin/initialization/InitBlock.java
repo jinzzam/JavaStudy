5e9901b6d34bb8dd604e5279ab5431af2a75f1ca
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2017-12-31 10:31:07 +0900

    explicit initialization & initialization block (how to use)

---
+++ JavaStudy/src/com/youjin/initialization/InitBlock.java
@@ -0,0 +1,11 @@
+package com.youjin.initialization;
+
+public class InitBlock {
+	static {
+		/*클래스 초기화블럭*/
+	}
+	
+	{
+		/*인스턴스 초기화블럭*/
+	}
+}
---
+++ JavaStudy/src/com/youjin/initialization/explicit.java
@@ -0,0 +1,6 @@
+package com.youjin.initialization;
+
+public class explicit {		//명시적 초기화
+	int door = 4;
+	Engine e = new Engine();
+}
