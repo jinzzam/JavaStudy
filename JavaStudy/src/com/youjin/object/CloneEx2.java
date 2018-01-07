0c9621f0431b24dfeaf16dc4e328309f2b256df7
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2018-01-07 14:00:07 +0900

    array clone

---
+++ JavaStudy/src/com/youjin/object/CloneEx2.java
@@ -0,0 +1,20 @@
+package com.youjin.object;
+
+import java.util.Arrays;
+
+public class CloneEx2 {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		int[] arrClone = arr.clone();
+		int[] arrClone2 = new int[arr.length];
+		System.arraycopy(arr, 0, arrClone2, 0, arr.length);
+		arrClone[0] = 6;
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arrClone));
+		System.out.println(Arrays.toString(arrClone2));
+	}
+
+}
